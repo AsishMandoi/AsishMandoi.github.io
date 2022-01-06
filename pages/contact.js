9d6d8ae0cfdb5b75824830f10ff17e10df4af6c2
Asish Mandoi <luckyasish13@gmail.com>
2022-01-06 13:01:37 +0530

    added one-page resume

--- pages/contact.js
+++ pages/contact.js
@@ -14,21 +14,21 @@ export default function Contact() {
       {/* <h2 className={utilStyles.headingMd}>You can reach me on:</h2> */}
         <ul className={utilStyles.list}>
             <li className={utilStyles.listItem}>
-              <div id='contact'>Email</div>
-              <small>
+              <div id='contact-item'>Email</div>
+              <small id='contact-text'>
                 <a href="mailto:akmandoi@iitk.ac.in">akmandoi@iitk.ac.in</a><br />
                 <a href="mailto:luckyasish13@gmail.com">luckyasish13@gmail.com</a><i> (if not from IIT Kanpur)</i>
               </small>
             </li>
             <li className={utilStyles.listItem}>
-              <div id='contact'><a href={`https://www.linkedin.com/in/asish-mandoi-4178581b4/`}>LinkedIn</a></div>
-              <small>
+              <div id='contact-item'><a href={`https://www.linkedin.com/in/asish-mandoi-4178581b4/`}>LinkedIn</a></div>
+              <small id='contact-text'>
                 Send me a connection request if we are not connected.
               </small>
             </li>
             <li className={utilStyles.listItem}>
-              <div id='contact'><a href={`https://www.facebook.com/asish.mandoi`}>Facebook</a></div>
-              <small>
+              <div id='contact-item'><a href={`https://www.facebook.com/asish.mandoi`}>Facebook</a></div>
+              <small id='contact-text'>
                 Ping me on Messenger.
               </small>
             </li>
